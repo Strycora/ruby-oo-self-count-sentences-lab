b45b7c3bd581074f921b3e9af5bfea8bd442491d
Vikram Chatterjee <strycora@gmail.com>
2020-11-18 11:05:27 -0700

    commit tree fcd0aae9bbb85a7d06044484d808887d5a48772b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    counter = 0
+    self.split(/[.!?]/).each do |sentence|
+      if sentence == ""
+        next
+      else
+      counter += 1
+      end
+    end
+    counter
   end
 end
